1bbfa14d6119887c21d9d4e2fcf9e793197bc9db
Camila <mariacamilacalle1@gmail.com>
2024-11-21 11:01:34 -0500

    Define enums for color palette

---
+++ src/app/styles/colors.ts
@@ -0,0 +1,65 @@
+export enum COLORS {
+ // Neutral colors
+ Neutral_25 = "#F8FAF7",
+ Neutral_50 = "#D9DEE8",
+ Neutral_100 = "#C0C8D8",
+ Neutral_200 = "#A6B2C9",
+ Neutral_300 = "#8D9CB9",
+ Neutral_400 = "#7486AA",
+ Neutral_500 = "#55678B",
+ Neutral_600 = "#465472",
+ Neutral_700 = "#364259",
+ Neutral_800 = "#272F3F",
+ Neutral_900 = "#181D27",
+
+ // Primary colors
+ Primary_50 = "#B9E250",
+ Primary_100 = "#ABDD2C",
+ Primary_200 = "#94C11F",
+ Primary_300 = "#789E1A",
+ Primary_400 = "#5E7B14",
+
+ // Secondary colors
+ Secondary_50 = "#7E4F25",
+ Secondary_100 = "#9AD6AA",
+ Secondary_200 = "#6EC485",
+ Secondary_300 = "#49B265",
+ Secondary_400 = "#419F5A",
+ Secondary_500 = "#D8ECFF",
+ Secondary_700 = "#29BBE1",
+ Secondary_800 = "#0063BD",
+
+ // Success colors
+ Success_50 = "#D4EDDB",
+ Success_100 = "#9AD6AA",
+ Success_200 = "#6EC485",
+ Success_300 = "#49B265",
+ Success_400 = "#419F5A",
+ Success_500 = "#35824A",
+ Success_600 = "#296539",
+ Success_700 = "#1C3329",
+
+ // Warning colors
+ Warning_50 = "#FFF0D2",
+ Warning_100 = "#FFE4AE",
+ Warning_200 = "#FFD685",
+ Warning_300 = "#FFC95C",
+ Warning_400 = "#FFBB33",
+ Warning_500 = "#E09600",
+ Warning_600 = "#B87A00",
+ Warning_700 = "#895B00",
+
+ // Error colors
+ Error_50 = "#F8DDDD",
+ Error_100 = "#F1BBBB",
+ Error_200 = "#E78888",
+ Error_300 = "#DB5757",
+ Error_400 = "#D32F2F",
+ Error_500 = "#B92727",
+ Error_600 = "#871D1D",
+ Error_700 = "#452626",
+
+ // Shade colors
+ Shade_0 = "#FFFFFF",
+ Shade_100 = "#000000",
+}
