df23b25572201cbbbb7e8bfe59a571342a62b8a2
Camila <mariacamilacalle1@gmail.com>
2024-11-28 10:11:05 -0500

    Page.tsx included Hero and Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { Poppins } from '@next/font/google'
+//import Image from "next/image";
+//import styles from "./page.module.css";
+import { Poppins } from 'next/font/google';
+import Header from './components/layout/Header/Header';
+import Hero from "./home/Hero/Hero";
 
 const poppins = Poppins({
   weight: ['300', '400', '600', '700'],
   subsets: ['latin'],
-})
-
+});
 
 export default function Home() {
   return (
-   <main className= {poppins.className}>
-    <p>hola</p>
-
-   </main>
-  
-  );
+    <main className={poppins.className}>
+      <Header></Header>
+      <Hero></Hero>
+      <p>hola</p>
+    </main>
+  )
 }
