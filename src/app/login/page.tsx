c83588a541dcbaae8d285cbde12b741a895844ac
Maria Camila Calle <mariacamilacalle1@gmail.com>
2025-02-12 11:41:59 -0500

    Add POST request for user authentication (/login)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,38 +5,59 @@ import Image from "next/image";
 import React, { useState } from "react";
 import Link from "next/link";
 import InputField from "components/layout/InputField/InputField";
-
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
 
-
-const [formData,setFormData] = useState({
+  const router = useRouter();
+  
+  const [formData, setFormData] = useState({
     email: "",
     password: "",
-    
+  });
+
+  const [message, setMessage] = useState("")
+
+  const handleChange =  (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+     setFormData({
+      ...formData,
+      [name]: value,
     });
-    
-    const handleChange =( e: React.ChangeEvent<HTMLInputElement> ) =>{
-    
-        const {name,value} = e.target;
-    
-        setFormData ({
-            ...formData,
-            [name]:value,
-    
-        })
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try{
+
+      const response = await fetch("http://localhost:4000/login",{
+        method: "POST",
+        headers:{"Content-Type": "application/json",},
+        body: JSON.stringify({
+        email: formData.email,
+          password:formData.password,
+        }),
+      });
+
+      if(!response.ok){
+        throw new Error ("Credenciales incorrectas. Inténtalo de nuevo.")
+      }
+
+      const result = await response.json();
+      setMessage("Inicio de sesión exitoso. Redirigiendo...");
+
     }
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log("Datos enviados:", formData);
-      };
-    
-    
+   catch (err: any) {
+    setMessage(err.message);
+  }
+
+  };
+
   return (
     <div className={styles.full_container_login}>
-        
-        <div className={styles.left_container_login}>
+      <div className={styles.left_container_login}>
         <Image
           src="/img/portal.svg"
           alt="portal_leaves_login"
@@ -45,107 +66,102 @@ const [formData,setFormData] = useState({
           className={styles.portal_login}
         />
         <h2 className={styles.title_login}>Tu Portal a la Naturaleza</h2>
-
-        </div>
-
-
-
-
-    
-        <div className={styles.right_container_login}>
-
-        <div className={styles.icon_top_login}>
-
-        <Image
-         src="/icons/Vector.svg"
-         alt="close_icon"
-         width={18.68}
-         height={18.68}
-         className={styles.closeIcon}
-       ></Image>
-</div>
-            
-
-     
-      <div className={styles.logo_title_login}>
-        <Image
-          src="/img/big_logo.svg"
-          alt="vivero_pasoancho_logo"
-          width={296}
-          height={169}
-          className={styles.logo_form}
-        />
-        <p className={styles.login}>Iniciar sesión</p>
       </div>
 
-      <div className={styles.form_items}>
-
-<form onSubmit={handleSubmit} className={styles.register_form}>
-      <InputField   
-      label="Email"
-      name="email"
-  type="Email"
-  placeholder = "Ingrese su email"
-  required = {true}
-  value= {formData.email}
-  onChange= {handleChange}
-  
-  ></InputField>
-
-
-<InputField   
-      label="Contraseña"
-  type= "password"
-    name= "password"
-  placeholder = "Ingrese su contraseña"
-  required = {true}
-  value= {formData.password}
-  onChange={handleChange} ></InputField>
-
-<p className={styles.green_text}>¿Olvido su contraseña?</p>
-
-<button type="submit" className={styles.submitButton}>
-          Iniciar sesión
-        </button>
-
-        <div className={styles.divider_text}>O inicia sesión con</div>
-
-
-        <div className={styles.google_microsoft_container}>
- <div className={styles.icons_container}>
-           
-            <Image
-          src="/icons/google.svg"
-          alt="burguer_menu"
-          width={24}
-          height={24}
-          className={styles.menuIcon}
-        ></Image>
-         <p className={styles.text_google_microsoft}>Google</p>
+      <div className={styles.right_container_login}>
+        <div className={styles.icon_top_login}>
+          <Image
+            src="/icons/Vector.svg"
+            alt="close_icon"
+            width={18.68}
+            height={18.68}
+            className={styles.closeIcon}
+          ></Image>
         </div>
 
-        <div className={styles.icons_container}>
-        <Image
-          src="/icons/microsoft_icon.svg"
-          alt="burguer_menu"
-          width={24}
-          height={24}
-          className={styles.menuIcon}
-        ></Image>
-        <p className={styles .text_google_microsoft}>Microsoft</p>
+        <div className={styles.logo_title_login}>
+          <Image
+            src="/img/big_logo.svg"
+            alt="vivero_pasoancho_logo"
+            width={296}
+            height={169}
+            className={styles.logo_form}
+          />
+          <p className={styles.login}>Iniciar sesión</p>
         </div>
-            
+
+        <div className={styles.form_items}>
+          <form onSubmit={handleSubmit} className={styles.register_form}>
+            <InputField
+              label="Email"
+              name="email"
+              type="Email"
+              placeholder="Ingrese su email"
+              required={true}
+              value={formData.email}
+              onChange={handleChange}
+            ></InputField>
+
+            <InputField
+              label="Contraseña"
+              type="password"
+              name="password"
+              placeholder="Ingrese su contraseña"
+              required={true}
+              value={formData.password}
+              onChange={handleChange}
+            ></InputField>
+
+            <p className={styles.green_text}>¿Olvido su contraseña?</p>
+
+            <button type="submit" className={styles.submitButton}>
+              Iniciar sesión
+            </button>
+
+            <div className={styles.divider_text}>O inicia sesión con</div>
+
+            <div className={styles.google_microsoft_container}>
+              <div className={styles.icons_container}>
+                <Image
+                  src="/icons/google.svg"
+                  alt="burguer_menu"
+                  width={24}
+                  height={24}
+                  className={styles.menuIcon}
+                ></Image>
+                <p className={styles.text_google_microsoft}>Google</p>
+              </div>
+
+              <div className={styles.icons_container}>
+                <Image
+                  src="/icons/microsoft_icon.svg"
+                  alt="burguer_menu"
+                  width={24}
+                  height={24}
+                  className={styles.menuIcon}
+                ></Image>
+                <p className={styles.text_google_microsoft}>Microsoft</p>
+              </div>
+            </div>
+            <Link href="/registrar">
+              <p className={styles.new_account}>
+                ¿No tienes una cuenta aún?{" "}
+                <span className={styles.green_text}>Registrate aquí.</span>
+              </p>
+            </Link>
+            <p className={styles.small_text}>
+              Al continuar acepta nuestros{" "}
+              <span className={styles.small_text_form}>
+                Términos y Condiciones{" "}
+              </span>
+              y las{" "}
+              <span className={styles.small_text_form}>
+                Políticas de Tratamiento de Datos.
+              </span>
+            </p>
+          </form>
         </div>
-        <Link href="/registrar">
-        <p className={styles.new_account}>¿No tienes una cuenta aún? <span className={styles.green_text}>Registrate aquí.</span></p>
-        </Link>
-        <p className={styles.small_text}>Al continuar acepta nuestros <span className={styles.small_text_form}>Términos y Condiciones </span>y las <span className={styles.small_text_form}>Políticas de Tratamiento de Datos.</span></p>
-
-
-</form>
-</div>
-
-    </div>
+      </div>
     </div>
   );
 };
