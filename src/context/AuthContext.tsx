26849d668eb5419ed6eb014a0529c2fcc1eb0f99
Maria Camila Calle <mariacamilacalle1@gmail.com>
2025-02-17 11:31:57 -0500

    create context file

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+interface AuthContextType {
+  user: { email: string; name: string } | null;
+  login: (userData: { email: string; name: string }) => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<{ email: string; name: string } | null>(null);
+
+  useEffect(() => {
+    // Recuperar el usuario del localStorage al cargar la app
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const login = (userData: { email: string; name: string }) => {
+    setUser(userData);
+    localStorage.setItem("user", JSON.stringify(userData));
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
