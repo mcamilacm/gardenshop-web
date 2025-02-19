ba9a7dbe2130bd6e7611c2f3380e1e7d0ccdfdc3
Maria Camila Calle <mariacamilacalle1@gmail.com>
2025-02-19 10:30:27 -0500

    InputField correction

--- src/app/registrar/page.tsx
+++ src/app/registrar/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import React, { useState } from "react";
 import Link from "next/link";
 import styles from "./register.module.css";
-import InputField from "components/layout/InputField/InputField";
+import InputField from "@/app/components/layout/InputField/InputField";
 
 const RegisterPage = () => {
 
